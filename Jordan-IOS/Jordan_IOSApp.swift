33725814ec6aea55b8374d6636200295a607d9ec
jmurphy5613 <jmurphy5613@gmail.com>
2022-09-29 20:55:53 -0400

    Initial Commit

---
+++ Jordan-IOS/Jordan_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jordan_IOSApp.swift
+//  Jordan-IOS
+//
+//  Created by John Murphy on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Jordan_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
